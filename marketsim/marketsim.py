ffd959e83e7a76257a723fa8df151d4b0f255de1
Danielle LaMotto <lamottodanielle@gmail.com>
2023-10-14 16:40:12 -0400

    ml4t code

--- marketsim/marketsim.py
+++ marketsim/marketsim.py
@@ -22,25 +22,27 @@ GT honor code violation.
 -----do not edit anything above this line---  		  	   		  		 		  		  		    	 		 		   		 		  
   		  	   		  		 		  		  		    	 		 		   		 		  
 Student Name: Tucker Balch (replace with your name)  		  	   		  		 		  		  		    	 		 		   		 		  
-GT User ID: tb34 (replace with your User ID)  		  	   		  		 		  		  		    	 		 		   		 		  
-GT ID: 900897987 (replace with your GT ID)  		  	   		  		 		  		  		    	 		 		   		 		  
+GT User ID: dlamotto3 (replace with your User ID)  		  	   		  		 		  		  		    	 		 		   		 		  
+GT ID: 903951588 (replace with your GT ID)  		  	   		  		 		  		  		    	 		 		   		 		  
 """  		  	   		  		 		  		  		    	 		 		   		 		  
   		  	   		  		 		  		  		    	 		 		   		 		  
-import datetime as dt  		  	   		  		 		  		  		    	 		 		   		 		  
-import os  		  	   		  		 		  		  		    	 		 		   		 		  
+import datetime as dt
   		  	   		  		 		  		  		    	 		 		   		 		  
 import numpy as np  		  	   		  		 		  		  		    	 		 		   		 		  
   		  	   		  		 		  		  		    	 		 		   		 		  
 import pandas as pd  		  	   		  		 		  		  		    	 		 		   		 		  
-from util import get_data, plot_data  		  	   		  		 		  		  		    	 		 		   		 		  
-  		  	   		  		 		  		  		    	 		 		   		 		  
-  		  	   		  		 		  		  		    	 		 		   		 		  
+from util import get_data, plot_data
+
+
+def author(self):
+    return 'dlamotto3'
+
 def compute_portvals(  		  	   		  		 		  		  		    	 		 		   		 		  
     orders_file="./orders/orders.csv",  		  	   		  		 		  		  		    	 		 		   		 		  
     start_val=1000000,  		  	   		  		 		  		  		    	 		 		   		 		  
     commission=9.95,  		  	   		  		 		  		  		    	 		 		   		 		  
     impact=0.005,  		  	   		  		 		  		  		    	 		 		   		 		  
-):  		  	   		  		 		  		  		    	 		 		   		 		  
+):
     """  		  	   		  		 		  		  		    	 		 		   		 		  
     Computes the portfolio values.  		  	   		  		 		  		  		    	 		 		   		 		  
   		  	   		  		 		  		  		    	 		 		   		 		  
@@ -54,29 +56,64 @@ def compute_portvals(
     :type impact: float  		  	   		  		 		  		  		    	 		 		   		 		  
     :return: the result (portvals) as a single-column dataframe, containing the value of the portfolio for each trading day in the first column from start_date to end_date, inclusive.  		  	   		  		 		  		  		    	 		 		   		 		  
     :rtype: pandas.DataFrame  		  	   		  		 		  		  		    	 		 		   		 		  
-    """  		  	   		  		 		  		  		    	 		 		   		 		  
-    # this is the function the autograder will call to test your code  		  	   		  		 		  		  		    	 		 		   		 		  
-    # NOTE: orders_file may be a string, or it may be a file object. Your  		  	   		  		 		  		  		    	 		 		   		 		  
-    # code should work correctly with either input  		  	   		  		 		  		  		    	 		 		   		 		  
-    # TODO: Your code here  		  	   		  		 		  		  		    	 		 		   		 		  
-  		  	   		  		 		  		  		    	 		 		   		 		  
-    # In the template, instead of computing the value of the portfolio, we just  		  	   		  		 		  		  		    	 		 		   		 		  
-    # read in the value of IBM over 6 months  		  	   		  		 		  		  		    	 		 		   		 		  
-    start_date = dt.datetime(2008, 1, 1)  		  	   		  		 		  		  		    	 		 		   		 		  
-    end_date = dt.datetime(2008, 6, 1)  		  	   		  		 		  		  		    	 		 		   		 		  
-    portvals = get_data(["IBM"], pd.date_range(start_date, end_date))   # Gets the prices
-    portvals = portvals[["IBM"]]  # remove SPY
-    rv = pd.DataFrame(index=portvals.index, data=portvals.values)
+    """
+    # this is the function the autograder will call to test your code
+    # NOTE: orders_file may be a string, or it may be a file object. Your
+    # code should work correctly with either input
+
+    # convert the orders to a dataframe
+    # Might need to do an if else statement for a string type???
+    orders_df = pd.read_csv(orders_file, index_col='Date', parse_dates=True, na_values=['nan'])
+
+    orders_df.sort_index(inplace=True)
+    # start date = first day in orders df, end date = last day in orders df
+    start_date = orders_df.index[0]
+    end_date = orders_df.index[-1]
+    # gets the list of symbols that are in the order sheet
+    symbols = orders_df['Symbol'].unique().tolist()
+    # initialize a dictionary of shares with each symbol starting with 0 shares
+    shares = {symbol: 0 for symbol in symbols}
+    # initialize our balance with our starting value (start_val)
     balance = start_val
-    number_of_shares = 0
+    # get the prices for our date range and symbols and clean the data, the dates are the index
+    adjusted_closing_prices = get_data(symbols, pd.date_range(start_date, end_date))
+    adjusted_closing_prices.fillna(method="ffill", inplace=True)  # fill missing data forward
+    adjusted_closing_prices.fillna(method="bfill", inplace=True)  # fill missing data backward
 
+    # initialize the empty dataframe to be returned
+    portvals = pd.DataFrame(columns=['Date', 'Balance'])
 
+    # iterates through the orders dataframe
+    for index, row in orders_df.iterrows():
+        symbol = row['Symbol']
+        order_type = row['Order']
+        num_shares = row['Shares']
 
-    # print(rv)
-    # port_vals = rv.sort_values(by='date')
-    return rv
-    # return portvals
-  		  	   		  		 		  		  		    	 		 		   		 		  
+        # get the current price of the symbol on the given day
+        current_price = adjusted_closing_prices.loc[index, symbol]
+        # get the amount the market would impact the price
+        impacted_amount = current_price * impact
+
+        if symbol in shares:
+            if order_type == 'BUY':
+                shares[symbol] += num_shares    # add the number of shares we are buying to our portfolio
+                # adjust the price of a share based on market impact of a buy
+                impacted_price = current_price + impacted_amount
+                price = num_shares * impacted_price     # the total price of all shares
+                balance -= price    # our cash amount minus the total price
+                balance -= commission   # our cash amount minus commission
+            else:
+                shares[symbol] -= num_shares     # add the number of shares we are selling from our portfolio
+                # adjust the price of a share based on market impact of a sell
+                impacted_price = current_price - impacted_amount
+                price = num_shares * impacted_price     # the total price of all shares
+                balance += price    # our cash amount plus the total price
+                balance -= commission   # our cash amount minus commission
+
+        portvals.loc[index] = [index, balance]  # the value of our portfolio for each trading day
+
+    return portvals
+
   		  	   		  		 		  		  		    	 		 		   		 		  
 def test_code():  		  	   		  		 		  		  		    	 		 		   		 		  
     """  		  	   		  		 		  		  		    	 		 		   		 		  
@@ -90,45 +127,46 @@ def test_code():
     sv = 1000000  		  	   		  		 		  		  		    	 		 		   		 		  
   		  	   		  		 		  		  		    	 		 		   		 		  
     # Process orders  		  	   		  		 		  		  		    	 		 		   		 		  
-    portvals = compute_portvals(orders_file=of, start_val=sv)  		  	   		  		 		  		  		    	 		 		   		 		  
-    if isinstance(portvals, pd.DataFrame):
-        portvals = portvals[portvals.columns[0]]  # just get the first column
-    else:
-        "warning, code did not return a DataFrame"
-  		  	   		  		 		  		  		    	 		 		   		 		  
-    # Get portfolio stats  		  	   		  		 		  		  		    	 		 		   		 		  
-    # Here we just fake the data. you should use your code from previous assignments.  		  	   		  		 		  		  		    	 		 		   		 		  
-    start_date = dt.datetime(2008, 1, 1)  		  	   		  		 		  		  		    	 		 		   		 		  
-    end_date = dt.datetime(2008, 6, 1)  		  	   		  		 		  		  		    	 		 		   		 		  
-    cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = [  		  	   		  		 		  		  		    	 		 		   		 		  
-        (allocated_daily_returns[-1] / allocated_daily_returns[0])-1,
-        allocated_daily_returns.mean(),
-        allocated_daily_returns.std(),
-        math.sqrt(252) * ((allocated_daily_returns.mean())/allocated_daily_returns.std()), # a risk-free rate of 0
-    ]  		  	   		  		 		  		  		    	 		 		   		 		  
-    cum_ret_SPY, avg_daily_ret_SPY, std_daily_ret_SPY, sharpe_ratio_SPY = [  		  	   		  		 		  		  		    	 		 		   		 		  
-        0.2,  		  	   		  		 		  		  		    	 		 		   		 		  
-        0.01,  		  	   		  		 		  		  		    	 		 		   		 		  
-        0.02,  		  	   		  		 		  		  		    	 		 		   		 		  
-        1.5,  		  	   		  		 		  		  		    	 		 		   		 		  
-    ]  		  	   		  		 		  		  		    	 		 		   		 		  
-  		  	   		  		 		  		  		    	 		 		   		 		  
-    # Compare portfolio against $SPX  		  	   		  		 		  		  		    	 		 		   		 		  
-    print(f"Date Range: {start_date} to {end_date}")  		  	   		  		 		  		  		    	 		 		   		 		  
-    print()  		  	   		  		 		  		  		    	 		 		   		 		  
-    print(f"Sharpe Ratio of Fund: {sharpe_ratio}")  		  	   		  		 		  		  		    	 		 		   		 		  
-    print(f"Sharpe Ratio of SPY : {sharpe_ratio_SPY}")  		  	   		  		 		  		  		    	 		 		   		 		  
-    print()  		  	   		  		 		  		  		    	 		 		   		 		  
-    print(f"Cumulative Return of Fund: {cum_ret}")  		  	   		  		 		  		  		    	 		 		   		 		  
-    print(f"Cumulative Return of SPY : {cum_ret_SPY}")  		  	   		  		 		  		  		    	 		 		   		 		  
-    print()  		  	   		  		 		  		  		    	 		 		   		 		  
-    print(f"Standard Deviation of Fund: {std_daily_ret}")  		  	   		  		 		  		  		    	 		 		   		 		  
-    print(f"Standard Deviation of SPY : {std_daily_ret_SPY}")  		  	   		  		 		  		  		    	 		 		   		 		  
-    print()  		  	   		  		 		  		  		    	 		 		   		 		  
-    print(f"Average Daily Return of Fund: {avg_daily_ret}")  		  	   		  		 		  		  		    	 		 		   		 		  
-    print(f"Average Daily Return of SPY : {avg_daily_ret_SPY}")  		  	   		  		 		  		  		    	 		 		   		 		  
-    print()  		  	   		  		 		  		  		    	 		 		   		 		  
-    print(f"Final Portfolio Value: {portvals[-1]}")  		  	   		  		 		  		  		    	 		 		   		 		  
+    portvals = compute_portvals(orders_file=of, start_val=sv)
+    print(portvals)
+    # if isinstance(portvals, pd.DataFrame):
+    #     portvals = portvals[portvals.columns[0]]  # just get the first column
+    # else:
+    #     "warning, code did not return a DataFrame"
+    #
+    # # Get portfolio stats
+    # # Here we just fake the data. you should use your code from previous assignments.
+    # start_date = dt.datetime(2008, 1, 1)
+    # end_date = dt.datetime(2008, 6, 1)
+    # cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = [
+    #     (allocated_daily_returns[-1] / allocated_daily_returns[0])-1,
+    #     allocated_daily_returns.mean(),
+    #     allocated_daily_returns.std(),
+    #     math.sqrt(252) * ((allocated_daily_returns.mean())/allocated_daily_returns.std()), # a risk-free rate of 0
+    # ]
+    # cum_ret_SPY, avg_daily_ret_SPY, std_daily_ret_SPY, sharpe_ratio_SPY = [
+    #     0.2,
+    #     0.01,
+    #     0.02,
+    #     1.5,
+    # ]
+  	#
+    # # Compare portfolio against $SPX
+    # print(f"Date Range: {start_date} to {end_date}")
+    # print()
+    # print(f"Sharpe Ratio of Fund: {sharpe_ratio}")
+    # print(f"Sharpe Ratio of SPY : {sharpe_ratio_SPY}")
+    # print()
+    # print(f"Cumulative Return of Fund: {cum_ret}")
+    # print(f"Cumulative Return of SPY : {cum_ret_SPY}")
+    # print()
+    # print(f"Standard Deviation of Fund: {std_daily_ret}")
+    # print(f"Standard Deviation of SPY : {std_daily_ret_SPY}")
+    # print()
+    # print(f"Average Daily Return of Fund: {avg_daily_ret}")
+    # print(f"Average Daily Return of SPY : {avg_daily_ret_SPY}")
+    # print()
+    # print(f"Final Portfolio Value: {portvals[-1]}")
   		  	   		  		 		  		  		    	 		 		   		 		  
   		  	   		  		 		  		  		    	 		 		   		 		  
 if __name__ == "__main__":  		  	   		  		 		  		  		    	 		 		   		 		  
